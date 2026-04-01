19b1e2e6aac2ef8674b0b2ef5e3e9e5edcaadd43
Mohamed Khalef <medyacoubkhalef@gmail.com>
2026-04-01 14:40:38 +0000

    Initial version

--- core/settings.py
+++ core/settings.py
@@ -179,6 +179,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://m2-engage.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
